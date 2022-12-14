ac320a52f1274d941bd29493d5eecd037a504918
kanmongkhon buasa <44879028+kanmongkhon97@users.noreply.github.com>
2022-12-14 10:45:17 +0700

    Update url.py

--- url.py
+++ url.py
@@ -1,8 +1,6 @@
 import requests
 URL = "http://m.jxyuging.com/shares/2022/1208/232019.html"
 response = requests.get(URL)
-i = 0
-for x in range(6):
-    i + 1
-    print("requests : %2d" % (i))
+for x in range(10):
+    print("requests : %2d" % (x))
     open("232019.html", "wb").write(response.content)
